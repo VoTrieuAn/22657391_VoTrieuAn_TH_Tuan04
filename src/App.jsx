7e6b872ca907bf64546bd2a4b2e9068c11c589d5
VoTrieuAn <voan2068@gmail.com>
2025-04-23 01:56:58 +0700

    Hoàn thành bài tập tuần 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LaiXuat from "./LaiXuat/LaiXuat";
 import { Routes, Route } from "react-router-dom";
@@ -13,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/laixuat" element={<LaiXuat />} />
         <Route path="/" element={<Chefify />} />
-        <Route path="/2" element={<Chefify2/>}></Route>
+        <Route path="/2" element={<Chefify2 />}></Route>
       </Routes>
     </div>
   );
